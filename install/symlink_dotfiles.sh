f51cf2cbff2cad8aef3b6ffc061ffba10dff479d
Joshua Steele <joshukraine@gmail.com>
2015-03-31 15:27:01 +0300

    Add symlink_dotfiles.sh and vundle.sh

---
+++ install/symlink_dotfiles.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+################################################################################
+# symlink_dotfiles.sh
+#
+# This script backs up any old dotfiles on the system, and symlinks the new ones
+# to their proper place in the home folder.
+################################################################################
+
+set -e # Terminate script if anything exits with a non-zero value
+set -u # Prevent unset variables
+
+################################################################################
+# Set some variables
+################################################################################
+
+DOTFILES=$HOME/dotfiles
+OLD_DOTFILES_BACKUP=$HOME/old_dotfiles_backup
+files="gemrc gitignore gitconfig tmux.conf railsrc vimrc zshrc"
+
+################################################################################
+# Back up old dotfiles if needed
+################################################################################
+
+if [[ -d $DOTFILES ]]; then
+  fancy_echo "Backing up old dotfiles to $HOME/old_dotfiles_backup..."
+  rm -rf $OLD_DOTFILES_BACKUP
+  cp -R $DOTFILES $OLD_DOTFILES_BACKUP
+fi
+
+################################################################################
+# Symklink new dotfiles to $HOME
+################################################################################
+
+fancy_echo "Creating symlinks..."
+for file in $files; do
+  fancy_echo "-> Linking $DOTFILES/$file to $HOME/.$file..."
+  ln -nfs "$DOTFILES/$file" "$HOME/.$file"
+done
+
---
+++ install/vundle.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+################################################################################
+# vundle.sh
+#
+# This script installs Vundle.
+################################################################################
+
+if [[ -d $HOME/.vim/bundle ]]; then
+  rm -rf $HOME/.vim/bundle
+fi
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp -R $DOTFILES/vim/colors $HOME/.vim # So vim won't complain about solarized not being found.
+vim +PluginInstall +qall
+rm -rf $HOME/.vim/colors
