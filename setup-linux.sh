327ef4035c194324f70f0dbb0b2ae2b72274d0c2
Joshua Steele <joshukraine@gmail.com>
2015-04-07 08:10:47 +0300

    Correct path for Terminator config copy command.

--- setup-linux.sh
+++ setup-linux.sh
@@ -179,7 +179,7 @@ fancy_echo "Cloning joshukraine/dotfiles repo to ${DOTFILES_DIR} ..."
 git clone -b linux https://github.com/joshukraine/dotfiles.git $DOTFILES_DIR
 
 fancy_echo "Copying Terminator profile into place..."
-cp -f terminator/config $HOME/.config/terminator/config
+cp -f $DOTFILES_DIR/terminator/config $HOME/.config/terminator/config
 
 source "$DOTFILES_DIR/install/symlink_dotfiles.sh"
 fancy_echo "Dotfiles setup complete!"
